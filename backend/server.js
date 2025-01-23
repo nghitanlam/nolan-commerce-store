3beddca73880f9ee0f0a5c57f1451e0c2a644845
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-23 13:42:07 +0700

    BE_ mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
-
 import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`✅ Server is running on port: ${PORT}`);
 });
