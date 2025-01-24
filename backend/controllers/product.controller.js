e2b9f122c5c2822b3c4ba3e8755bd49dbcf30298
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-24 21:44:02 +0700

    BE_ get all products API added

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,13 @@
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ products });
+  } catch (error) {
+    console.log(`❌ Error in getAllProducts controller ${error.message}`);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
