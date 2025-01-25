4318063be93587220b2f85bac042c59b44245bc1
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-25 16:32:24 +0700

    BE_ get featured products added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -6,6 +7,33 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log(`❌ Error in getAllProducts controller ${error.message}`);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // if not in reids, get from mongodb
+    // lean() is return javasciprt object, not mongodb document => good performance
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    // store in redis for future quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log(`❌ Error in getFeaturedProducts controller ${error.message}`);
     res
       .status(500)
       .json({ message: "Internal Server Error", error: error.message });
